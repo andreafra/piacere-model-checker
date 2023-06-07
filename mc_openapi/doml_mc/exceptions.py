e54ecd56de0521a3cd94d24981102244f257eb0a
Andrea Franchini <hello@andreafranchini.com>
2023-06-07 09:29:12 +0200

    fix exceptions constructor

--- mc_openapi/doml_mc/exceptions.py
+++ mc_openapi/doml_mc/exceptions.py
@@ -2,22 +2,22 @@ from mc_openapi.doml_mc.intermediate_model.metamodel import DOMLVersion
 
 
 class BadDOMLException(Exception):
-    def __init__(self, message: str, *args: object) -> None:
+    def __init__(self, message: str = None, *args: object) -> None:
         super().__init__(*args)
         self.errors = message or "The submitted DOML contains some kind of error."
 
 class UnsupportedDOMLVersionException(Exception):
-    def __init__(self, message: str, *args: object) -> None:
+    def __init__(self, message: str = None, *args: object) -> None:
         super().__init__(*args)
         self.errors = message or "The DOML version is not supported."
 
 class MissingInfrastructureLayerException(Exception):
-    def __init__(self, message: str, *args: object) -> None:
+    def __init__(self, message: str = None, *args: object) -> None:
         super().__init__(*args)
         self.errors = message or "Abstract infrastructure layer is missing from DOML."
 
 class NoActiveConcreteLayerException(Exception):
-    def __init__(self, message: str, *args: object) -> None:
+    def __init__(self, message: str = None, *args: object) -> None:
         super().__init__(*args)
         self.errors = message or "No active concrete infrastructure layer has been specified in DOML."
 
