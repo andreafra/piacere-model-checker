1fa3b56a3134578133de451774e6364ac019bec0
Andrea Franchini <hello@andreafranchini.com>
2023-08-09 13:01:01 +0200

    Add validation for networks in HTML model checker

--- mc_openapi/doml_mc/network_address_validator/validate.py
+++ mc_openapi/doml_mc/network_address_validator/validate.py
@@ -1,3 +1,4 @@
+import logging
 from mc_openapi.doml_mc.intermediate_model import DOMLElement
 from mc_openapi.doml_mc.mc import ModelChecker
 from ipaddress import IPv4Address, IPv4Network
@@ -50,16 +51,20 @@ def validate_network_address(imc: ModelChecker):
             acc.append((subnet, IPv4Network(subnet_addr)))
             visit_subnet(subnet, acc)
 
-    def fix_invalid_address(address: any):
-        # TODO: Use a match statement or use regexps
+    def fix_invalid_address(address: any, net: DOMLElement, warning: list):
+        # TODO: Use a match statement or use regexps once syntax for CIDR/Address is clear
         if isinstance(address, str) and address.startswith('/'):
+            warning.append(("Network", f"Net '{net.user_friendly_name}' has an incomplete address: '{address}'. '0.0.0.0' has been temporarily assigned."))
             return f"{BASE_ADDR}{address}"
 
         return address
 
-    warnings = []
+    warnings: list[tuple[str, list[tuple[str, str]]]] = []
 
     for network in networks:
+
+        warning = []
+
         # Tuple(elem, cidr)
         subnets: list[tuple[DOMLElement, IPv4Network]] = []
         # Tuple(elem, address)
@@ -83,19 +88,21 @@ def validate_network_address(imc: ModelChecker):
                 if owner.id_ in [s.id_ for (s, _) in subnets] + [network.id_]:
                     if ((iface_address := get_attr(iface, ATTR_IFACE_ADDRESS))
                     and (owner_address := get_attr(owner, ATTR_NET_ADDRESS))):
-                        owner_address = fix_invalid_address(owner_address)
+                        owner_address = fix_invalid_address(owner_address, owner, warning)
                         iface_address = IPv4Address(iface_address)
                         owner_address = IPv4Network(owner_address)
                         # TODO: Remove?
-                        print(f"{iface.user_friendly_name} ({iface_address}) belongs to {owner.user_friendly_name} ({owner_address})? {iface_address in owner_address.hosts()}")
+                        logging.info(f"{iface.user_friendly_name} ({iface_address}) belongs to {owner.user_friendly_name} ({owner_address})? {iface_address in owner_address.hosts()}")
 
                         addresses.append((iface, iface_address))
                 else:
-                    # TODO: Convert to warning
-                    print(f"NetworkInterface '{iface.user_friendly_name}' does not belong to net '{owner.user_friendly_name}'.")
+                    logging.info(f"NetworkInterface '{iface.user_friendly_name}' does not belong to net '{owner.user_friendly_name}'.")
+                    warning.append((
+                        "Address", f"NetworkInterface '{iface.user_friendly_name}' does not belong to net '{owner.user_friendly_name}'."
+                    ))
 
         # Validate Network and Subnets
-        net_addr = fix_invalid_address( get_attr(network, ATTR_NET_ADDRESS) )
+        net_addr = fix_invalid_address(get_attr(network, ATTR_NET_ADDRESS), network, warning)
         # prepend 0.0.0.0 if starts with / i guess, print a warning
         print(f"{net_addr}\t{network.user_friendly_name}")
 
@@ -103,15 +110,27 @@ def validate_network_address(imc: ModelChecker):
 
         if len(subnets) > 0:
             for (obj, sn) in subnets:
-                # TODO: Convert to warning
-                print(f"{sn}\t{obj.user_friendly_name} belongs? {sn.subnet_of(net)}")
+                logging.info(f"{sn}\t{obj.user_friendly_name} belongs? {sn.subnet_of(net)}")
+                if not sn.subnet_of(net):
+                    warning.append((
+                        "Subnet", f"Subnet {obj.user_friendly_name} ({sn}) does not belong to net '{network.user_friendly_name}' ({net_addr})."
+                    ))
         else:
-            print("No subnets!")
+            print("No subnets found!")
 
         # Validate addresses (again)
         for (obj, addr) in addresses:
-            # TODO: Convert to warning
-            print(f"NetworkInterface '{obj.user_friendly_name}' ({addr}) belong to net '{network.user_friendly_name}' ({net_addr})? {addr in net.hosts()}")
+            if addr not in net.hosts():
+                warning.append((
+                    "Address", f"'{obj.user_friendly_name}' [{obj.class_}] ({addr}) does not belong to net '{network.user_friendly_name}' ({net_addr})."
+                ))
+            logging.info(f"NetworkInterface '{obj.user_friendly_name}' ({addr}) belong to net '{network.user_friendly_name}' ({net_addr})? {addr in net.hosts()}")
+
+        warnings.append((network.user_friendly_name, list(set(warning))))
+
+    return warnings
+        
+    
 
   
 
